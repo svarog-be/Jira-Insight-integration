cf53943088ad5583bb973ddbeb33816d733511e1
svarog-be <49800760+svarog-be@users.noreply.github.com>
2021-01-15 11:06:12 +0700

    Delete PS Get-Credential.ps1

--- PS Get-Credential.ps1
+++
@@ -1,8 +0,0 @@
-$Cred = Get-Credential
-$Cred.Password | ConvertFrom-SecureString | Set-Content ./pass.txt
-$Cred.UserName | Set-Content ./user.txt
-
-
-$username = Get-Content ./user.txt
-$pass = Get-Content ./pass.txt | ConvertTo-SecureString
-$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $pass
